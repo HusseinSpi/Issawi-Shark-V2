f90352d85ffc8dc02a9f895db30796ed50fe1b03
HusseinSpi <Huseeink852@gmail.com>
2024-07-07 00:48:59 +0300

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,16 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: "esnext",
-    outDir: "dist",
-    assetsDir: "assets",
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
   server: {
     mimeTypes: {
       "application/javascript": ["js"],
