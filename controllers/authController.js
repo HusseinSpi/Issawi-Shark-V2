663f82377d97a7a1a21cd443ba193736faad6ac4
HusseinSpi <Huseeink852@gmail.com>
2024-07-07 11:23:56 +0300

    d

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,18 +7,16 @@ const AppError = require("../utils/appError");
 const sendEmail = require("../utils/email");
 
 const signToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
+  return jwt.sign({ id }, "iam-love-a-cyber-security-very-much", {
+    expiresIn: "90d",
   });
 };
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
   const cookieOptions = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
+    expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+    httpOnly: false,
     sameSite: "Lax",
   };
   if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
