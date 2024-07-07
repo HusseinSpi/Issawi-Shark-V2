4dedce7a7f399aa08fc0ebaa4630ef0f89d47c7a
HusseinSpi <Huseeink852@gmail.com>
2024-07-07 17:09:36 +0300

    test

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
-const rateLimit = require("express-rate-limit");
-const helmet = require("helmet");
-const mongoSanitize = require("express-mongo-sanitize");
-const xss = require("xss-clean");
+// const rateLimit = require("express-rate-limit");
+// const helmet = require("helmet");
+// const mongoSanitize = require("express-mongo-sanitize");
+// const xss = require("xss-clean");
 const cors = require("cors");
 // const cookieParser = require("cookie-parser");
 
-const AppError = require("./utils/appError");
+// const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 const userRoutes = require("./routes/userRoutes");
 const projectRoutes = require("./routes/projectRoutes");
@@ -26,16 +26,16 @@ const app = express();
 // };
 
 app.use(cors());
-app.use(helmet());
+// app.use(helmet());
 // app.use(cookieParser());
 
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-}
+// if (process.env.NODE_ENV === "development") {
+//   app.use(morgan("dev"));
+// }
 
-app.use(express.json({ limit: "10kb" }));
-app.use(mongoSanitize());
-app.use(xss());
+app.use(express.json());
+// app.use(mongoSanitize());
+// app.use(xss());
 
 // app.use((req, res, next) => {
 //   res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
